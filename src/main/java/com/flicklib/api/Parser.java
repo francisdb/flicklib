4363efcfd4a7ce334815a50a9c3ab1bf607d17cd
Francis De Brabandere <francisdb@gmail.com>
2008-10-01 08:29:03 +0000

    apache license

--- src/main/java/com/flicklib/api/Parser.java
+++ src/main/java/com/flicklib/api/Parser.java
@@ -3,11 +3,11 @@
  *
  * Copyright (C) Francis De Brabandere
  *
- * Licensed under the GNU Lesser General Public License (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.gnu.org/licenses/lgpl.html
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
