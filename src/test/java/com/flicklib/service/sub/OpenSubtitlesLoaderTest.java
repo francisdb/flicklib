92164937f4142628acce56ff40dfe1e4f5f1675a
Francis De Brabandere <francisdb@gmail.com>
2009-01-11 17:41:05 +0000

    ignoring failing test because opesubtitles search is not that good any more...

--- src/test/java/com/flicklib/service/sub/OpenSubtitlesLoaderTest.java
+++ src/test/java/com/flicklib/service/sub/OpenSubtitlesLoaderTest.java
@@ -21,6 +21,7 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.Set;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -47,7 +48,14 @@ public class OpenSubtitlesLoaderTest {
         assertTrue(result.size() > 0);
         for(Subtitle sub:result){
             LOGGER.info(sub.getFileName());
-        }
+        }        
+    }
+    
+    @Test
+    @Ignore(value="The opensubtitles site search is failing for this, used to work though and the file is still there...")
+    public void testSearch2() throws Exception {
+    	SubtitlesLoader loader = new OpenSubtitlesLoader(new HttpSourceLoader(null));
+    	Set<Subtitle> result = loader.search("The.Science.of.Sleep.LIMITED.DVDRip.XViD.-iMBT.avi", null);
         result = loader.search("The.Science.of.Sleep.LIMITED.DVDRip.XViD", null);
         assertTrue(result.size() > 0);
         for(Subtitle sub:result){
