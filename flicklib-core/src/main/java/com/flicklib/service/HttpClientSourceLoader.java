3fa2e7632f02c3a79703f288e7a2fc91ec794591
Zsombor Gegesy <gzsombor@gmail.com>
2011-10-23 01:33:18 +0200

    A couple of upgrade:  - httpclient 3.1 -> httpcomponents 4.1.2  - jericho-html 3.1 -> 3.2  - slf4j 1.5.8 -> 1.6.3  - guice 1.0 -> 2.0  (no 3.0 in maven repo)  - logback-classic 0.9.17 -> 0.9.29 (no 0.9.30 in maven repo)
    JSON and OAuth seems a bit dated, but no more recent working version foundi

--- flicklib-core/src/main/java/com/flicklib/service/HttpClientSourceLoader.java
+++ flicklib-core/src/main/java/com/flicklib/service/HttpClientSourceLoader.java
@@ -1,21 +1,28 @@
 package com.flicklib.service;
 
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.MultiThreadedHttpConnectionManager;
-import org.apache.commons.httpclient.RedirectException;
-import org.apache.commons.httpclient.methods.GetMethod;
-import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.flicklib.module.FlicklibModule;
-import com.flicklib.tools.IOTools;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
 
@@ -23,51 +30,35 @@ public class HttpClientSourceLoader implements SourceLoader {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(HttpClientSourceLoader.class);
 
-	private final HttpClient client;
+	private final DefaultHttpClient client;
+	private final BasicHttpContext context;
 
 	@Inject
 	public HttpClientSourceLoader(@Named(value = FlicklibModule.HTTP_TIMEOUT) final Integer timeout) {
-		// http://hc.apache.org/httpclient-3.x/performance.html#Concurrent_execution_of_HTTP_methods
-		client = new HttpClient(new MultiThreadedHttpConnectionManager());
 
+	    ThreadSafeClientConnManager tm = new ThreadSafeClientConnManager();
+	        client = new DefaultHttpClient(tm); 
+
+	        context = new BasicHttpContext();
+	        
 		if (timeout != null) {
 			// wait max x sec
-			client.getParams().setSoTimeout(timeout);
-			//manager.getParams().setSoTimeout(timeout);
-			// LOGGER.info("Timeout = "+client.getParams().getSoTimeout());
+		    HttpConnectionParams.setSoTimeout(client.getParams(), timeout);
 		}
 	}
 
 	@Override
 	public Source loadSource(String url) throws IOException {
-		GetMethod httpMethod = null;
-		Reader is = null;
+		HttpResponse response = null;
 		try {
 			LOGGER.info("Loading " + url);
-			httpMethod = new GetMethod(url);
+			HttpGet httpMethod = new HttpGet(url);
 			//httpMethod.addRequestHeader("Content-Type","text/html; charset=UTF-8");
-			try {
-				client.executeMethod(httpMethod);
-			} catch (RedirectException ex) {
-				throw new IOException("Redirect problem: " + ex.getMessage(), ex);
-			}
-			LOGGER.debug("Finished loading at " + httpMethod.getURI().toString());
-			String responseCharset = httpMethod.getResponseCharSet();
-			LOGGER.info("Response charset = " + responseCharset);
-			if (responseCharset != null) {
-				is = new InputStreamReader(httpMethod.getResponseBodyAsStream(), responseCharset);
-			} else {
-				// default as described in: http://hc.apache.org/httpclient-3.x/charencodings.html
-				is = new InputStreamReader(httpMethod.getResponseBodyAsStream(), "ISO-8859-1");
-			}
-			String contentType = httpMethod.getResponseHeader("Content-Type").getValue();
-			// String contentType = URLConnection.guessContentTypeFromName(url)
-			return new Source(httpMethod.getURI().toString(), IOTools.readerToString(is), contentType, url);
+			response = client.execute(httpMethod, context);
+			
+			return buildSource(url, response, httpMethod);
 		} finally {
-			IOTools.close(is);
-			if (httpMethod != null) {
-				httpMethod.releaseConnection();
-			}
+			EntityUtils.consume(response.getEntity());
 		}
 	}
 
@@ -78,45 +69,40 @@ public class HttpClientSourceLoader implements SourceLoader {
 
 	@Override
 	public Source post(String url, Map<String, String> parameters, Map<String, String> headers) throws IOException {
-		PostMethod httpMethod = null;
-		Reader is = null;
+                HttpResponse response = null;
 		try {
 			LOGGER.info("Loading " + url);
-			httpMethod = new PostMethod(url);
-			if (parameters != null) {
+			HttpPost httpMethod = new HttpPost(url);
+			if (parameters != null && !parameters.isEmpty()) {
+			    List<NameValuePair> formparams = new ArrayList<NameValuePair>();
 				for (Entry<String, String> entry : parameters.entrySet()) {
-					httpMethod.addParameter(entry.getKey(), entry.getValue());
+					formparams.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));
 				}
+				UrlEncodedFormEntity form = new UrlEncodedFormEntity(formparams, "UTF-8");
+				httpMethod.setEntity(form);
 			}
-			if (headers != null) {
+			if (headers != null && !headers.isEmpty()) {
 				for (Entry<String, String> entry : headers.entrySet()) {
-					httpMethod.addRequestHeader(entry.getKey(), entry.getValue());
+					httpMethod.addHeader(entry.getKey(), entry.getValue());
 				}
 			}
-			try {
-				client.executeMethod(httpMethod);
-			} catch (RedirectException ex) {
-				throw new IOException("Redirect problem: " + ex.getMessage(), ex);
-			}
-			LOGGER.info("Finished loading at " + httpMethod.getURI().toString());
-			String responseCharset = httpMethod.getResponseCharSet();
-			LOGGER.info("Response charset = " + responseCharset);
-			if (responseCharset != null) {
-				is = new InputStreamReader(httpMethod.getResponseBodyAsStream(), responseCharset);
-			} else {
-				// default as described in: http://hc.apache.org/httpclient-3.x/charencodings.html
-				is = new InputStreamReader(httpMethod.getResponseBodyAsStream(), "ISO-8859-1");
-			}
-			String contentType = httpMethod.getResponseHeader("Content-Type").getValue();
-			// String contentType = URLConnection.guessContentTypeFromName(url)
-			return new Source(httpMethod.getURI().toString(), IOTools.readerToString(is), contentType, url);
+			response = client.execute(httpMethod, context);
+			return buildSource(url, response, httpMethod);
 		} finally {
-			IOTools.close(is);
-			if (httpMethod != null) {
-				httpMethod.releaseConnection();
-			}
+		    EntityUtils.consume(response.getEntity());
 		}
 
 	}
+
+    private Source buildSource(String url, HttpResponse response, HttpRequestBase httpMethod) throws IOException {
+        LOGGER.info("Finished loading at " + httpMethod.getURI().toString());
+        final HttpEntity entity = response.getEntity();
+        String responseCharset = EntityUtils.getContentCharSet(entity);
+        String contentType = EntityUtils.getContentMimeType(entity);
+        LOGGER.info("Response charset = " + responseCharset);
+        String content = EntityUtils.toString(entity);
+        // String contentType = URLConnection.guessContentTypeFromName(url)
+        return new Source(httpMethod.getURI().toString(), content, contentType, url);
+    }
 
 }
