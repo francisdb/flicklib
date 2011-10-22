4534d956073263b6601a0fbab3009f0d18a08fed
Zsombor Gegesy <gzsombor@gmail.com>
2011-10-22 11:28:27 +0200

    porthu response is dynamic, so it can varies from request to request

--- flicklib-core/src/test/java/com/flicklib/service/cache/CachesTest.java
+++ flicklib-core/src/test/java/com/flicklib/service/cache/CachesTest.java
@@ -3,8 +3,6 @@ package com.flicklib.service.cache;
 import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 
 import org.junit.Test;
 
@@ -18,29 +16,20 @@ import com.flicklib.service.UrlConnectionResolver;
  *
  */
 public class CachesTest {
-	
-	private static final String TEST_CITY_ID = "3372";
 
 	@Test
-	public void test() throws IOException{
-		String url = generateUrlForTitleSearch("Indiana Jones and the Kingdom of the Crystal Skull");
+	public void test() throws IOException {
+		String url = "http://api.blippr.com/v2/titles/652519.json";
 		SourceLoader empty = new MemoryCacheSourceLoader(new UrlConnectionResolver(5000));
 		SourceLoader empty2 = new MemoryCacheSourceLoader(new HttpClientSourceLoader(5000));
 		//HttpCache4J cache4j = new HttpCache4J();
-		
+
 		String emptyContent = empty.loadSource(url).getContent();
 		String emptyContent2 = empty2.loadSource(url).getContent();
 		//String cache4jContent = cache4j.get(url).getContent();
-		
+
 		assertEquals("two response should be the same", emptyContent, emptyContent2);
 		//assertEquals(emptyContent2, cache4jContent);
 	}
-	
-	protected String generateUrlForTitleSearch(String title) {
-        try {
-            return "http://port.hu/pls/ci/cinema.film_creator?i_text=" + URLEncoder.encode(title, "ISO-8859-2") + "&i_film_creator=1&i_city_id=" + TEST_CITY_ID;
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException("Encoding problem with UTF-8? " + e.getMessage(), e);
-        }
-    }
+
 }
